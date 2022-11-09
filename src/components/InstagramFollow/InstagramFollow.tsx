8517d45924d954c7ce904a8d21b51f6de099162e
Murad Kos <Murad.ishmurodov@gmail.com>
2022-11-09 16:25:22 +0100

    fix(deploy): v3 fixed image array in instragram section

--- src/components/InstagramFollow/InstagramFollow.tsx
+++ src/components/InstagramFollow/InstagramFollow.tsx
@@ -1,37 +1,21 @@
 import Image from 'next/image';
-import React from 'react';
 
 export default function InstagramFollow() {
-  const imgList = [];
-  const images = () => {
-    for (let i = 1; i < 7; i++) {
-      imgList.push(
-        <Image
-          key={i}
-          src={`/images/instagram/insta${i}.jpg`}
-          height={500}
-          width={500}
-          objectFit="cover"
-          alt="product"
-        />,
-      );
-    }
-  };
-  images();
+  const images = Array.from(Array(6), (x, index) => (
+    <Image
+      key={index + 1}
+      src={`/images/instagram/insta${index + 1}.jpg`}
+      height={500}
+      width={500}
+      objectFit="cover"
+      alt="product"
+    />
+  ));
   return (
     <section className="section">
       <h3 className="font-bold text-center mb-6">Follow us on Instagram</h3>
       <div className="m-0 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6">
-        {imgList}
-        {/* <div className="p-0">
-            <Image
-              src="/images/instagram/insta1.jpg"
-              height={500}
-              width={500}
-              objectFit="cover"
-              alt="product"
-            />
-          </div> */}
+        {images}
       </div>
     </section>
   );
